666e68d9d04b6a36965ed3aac02eb02aa34e50bd
aryan-madhavi <aryanmadhavi@yahoo.com>
2025-10-18 01:38:27 +0530

    Changed Repository Structure
    Created seperated folders for both applications within the project's ecosystem

---
+++ voicesewa_client/android/app/src/main/kotlin/com/example/voicesewa_client/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.voicesewa_client
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ voicesewa_worker/android/app/src/main/kotlin/com/example/voicesewa_worker/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.voicesewa_worker
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
