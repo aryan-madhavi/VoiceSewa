8a4d56ccc065113b9637b17dcf12043d5585b7c9
aryan-madhavi <aryanmadhavi@yahoo.com>
2025-11-07 19:13:20 +0530

    Refactor Bottom Navigation Bar

--- voicesewa_client/lib/widgets/core/bottom_navbar_widget.dart
+++ voicesewa_client/lib/widgets/core/bottom_navbar_widget.dart
@@ -11,36 +11,22 @@ class BottomNavBar extends ConsumerWidget {
     final currentTab = ref.watch(navTabProvider);
     final tabNotifier = ref.read(navTabProvider.notifier);
 
-    return Stack(
-      alignment: Alignment.center,
-      children: [
-        NavigationBar(
-          selectedIndex: currentTab.index,
-          onDestinationSelected: (index) {
-            tabNotifier.setTab(NavTab.values[index]);
-          },
-          destinations: AppConstants.pages.entries.map((entry) {
-            final icon = entry.value[0] as Widget;
-            final label = entry.value[1] as String;
-            return NavigationDestination(
-              icon: icon,
-              label: label,
-            );
-          }).toList(),
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            FloatingActionButton(
-              elevation: 4,
-              shape: const CircleBorder(),
-              onPressed: () {},
-              tooltip: 'Speak',
-              child: const Icon(Icons.mic),
-            ),
-          ],
-        ),
-      ],
+    return NavigationBar(
+      selectedIndex: currentTab.index,
+      onDestinationSelected: (index) {
+        tabNotifier.setTab(NavTab.values[index]);
+      },
+      destinations: AppConstants.pages.entries.map((entry) {
+        final icon = entry.value[0] as Widget;
+        final label = entry.value[1] as String;
+        return (label != '')
+            ? NavigationDestination(icon: icon, label: label)
+            : FloatingActionButton(
+                onPressed: () {},
+                tooltip: 'Speak',
+                child: const Icon(Icons.mic),
+              );
+      }).toList(),
     );
   }
 }
