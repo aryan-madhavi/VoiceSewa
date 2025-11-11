54a6dd320411f051e0ea6e764740a0b06c3d6222
Sankalp_Dawada <sankalpdawada@gmail.com>
2025-11-11 13:40:26 +0530

    Added your current raing on home page

--- voicesewa_worker/lib/screens/home/cards/rating.dart
+++ voicesewa_worker/lib/screens/home/cards/rating.dart
@@ -8,12 +8,46 @@ class Rating extends StatefulWidget {
 }
 
 class _RatingState extends State<Rating> {
+  double rating = 4.2;
+  int starCount = 5;
+
   @override
   Widget build(BuildContext context) {
-    return const Card(
-      child: Text(
-        "Rating Card"
+    return Padding(
+      padding: const EdgeInsets.all(10),
+      child: Card(
+      margin: EdgeInsets.all(10),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            // SizedBox(height: 20),
+            Text("Your Current Rating"),
+
+            // SizedBox(height: 20),
+            Center(
+              child: Row(
+                  children: List.generate(starCount, (index) => buildStar(context, index)),
+                ),
+            ),
+            // SizedBox(height: 20),
+              Text("$rating"),
+            // SizedBox(height: 20),
+          ],
+        ),
       ),
     );
   }
+Widget buildStar(BuildContext context, int index) {
+  Icon icon;
+  if (index >= rating) {
+    icon = Icon(Icons.star_border);
+  } else if (index > rating - 1 && index < rating) {
+    icon = Icon(Icons.star_half);
+  } else {
+    icon = Icon(Icons.star);
+  }
+  return icon;
 }
+}
+
