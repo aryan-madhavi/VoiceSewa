17657efa8334008dba8d11b09403e7db4250e9b1
sahilgangani17 <sg17022007@gmail.com>
2026-01-16 22:30:00 +0530

    feat: integrate Firebase authentication
    - Added firebase-admin dependency to package.json.
    - Created firebase.js to initialize Firebase Admin SDK.
    - Implemented middleware to verify Firebase tokens in authfirebasetoken.js.
    - Updated users route to use Firebase token verification for the '/users/me' endpoint.

---
+++ middleware/authfirebasetoken.js
@@ -0,0 +1,20 @@
+import { StatusCodes } from 'http-status-codes';
+import  admin from '../firebase.js';
+
+
+const verifyFirebaseToken = async (req, res, next) => {
+  const token = req.headers.authorization?.split("Bearer ")[1];
+
+  if (!token) return res.status(StatusCodes.UNAUTHORIZED).json({ error: "No token" });
+
+  try {
+    const decoded = await admin.auth().verifyIdToken(token);
+    req.user = decoded;
+    next();
+  } catch (err) {
+    res.status(StatusCodes.UNAUTHORIZED).json({ error: "Invalid Firebase token" });
+  }
+};
+
+
+export default verifyFirebaseToken;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import { StatusCodes } from 'http-status-codes';
+import verifyFirebaseToken from '../middleware/authfirebasetoken.js';
 
 const router = Router();
 
-router.get('/users/me', (req, res) => {
+router.get('/users/me', verifyFirebaseToken, (req, res) => {
   res.status(StatusCodes.NOT_FOUND)
   res.send({
     error: "Resource not Found",
