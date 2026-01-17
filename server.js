f6b81d57074861f84aff105cb8c2cffc28b5e36f
Aryan Madhavi <aryan.madhavi.00@gmail.com>
2026-01-17 22:52:02 +0530

    feat: add activation script for cross-shell development environment

--- server.js
+++ server.js
@@ -8,6 +8,12 @@ app.use(express.json());
 
 app.use('/api', UserRoutes);
 
+app.get('/api/deploy/status', (req,res) => {
+  res.status(200).json({
+    msg: `Server is running on http://oneplus-gm1901.orthrus-mahi.ts.net:${PORT}`,
+  })
+})
+
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 })
